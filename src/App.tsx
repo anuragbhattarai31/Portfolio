330599f539d9bffb7af5fe32c7cb259c4db5d0c8
Anurag Bhattarai <anuragbhattarai31@gmail.com>
2025-03-30 17:27:28 -0500

    Fixed game routes issue

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import TicTacToe from './components/TicTacToe';
 function App() {
   return (
     <ThemeProvider>
-      <Router basename="/Portfolio">
+      <Router basename="/Portfolio/">
         <div className="relative">
           <Header />
           <main className="pt-16">
